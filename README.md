c00d57c4d97fa7b0c78315e6814a3539c7e20844
Martin <martin.hajek1@tul.cz>
2023-12-14 20:10:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Name
+
+Short project description goes here.
+
+## Installation
+
+Instructions on how to install the project.
+
+## Usage
+
+Examples and instructions on how to use the project.
+
+## Contributing
+
+Guidelines for contributing to the project.
+
+## Contact
+
+Contact information for the project maintainer.
---
+++ RegulusDataSetter/main.py
@@ -0,0 +1,18 @@
+import requests
+
+def set_value():
+    # Your POST request code here
+    url = "https://regulusroute.tecomat.com/HOME.XML"
+    data = "__T4B9BD0CF_BOOL_i%3D1"
+    response = requests.post(url, data=data)
+    print(response.text)
+    if response.status_code == 200:
+        print("POST request successful")
+    else:
+        print("POST request failed")
+
+    
+
+if __name__ == '__main__':
+    set_value()
+    
