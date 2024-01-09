d6ffb3f080020caaf1e27f09859fd014d770e46d
Martin <martin.hajek1@tul.cz>
2024-01-09 16:03:51 +0100

    double change

--- RegulusDataSetter/main.py
+++ RegulusDataSetter/main.py
@@ -1,9 +1,9 @@
 import requests
 
-def set_value():
+def set_value(data):
     # Your POST request code here
     url = "http://192.168.0.116/HOME.XML"
-    data = "__T70BDAE66_REAL_.1f%3D20.0"
+    
     response = requests.post(url, data=data)
     print(response.text)
     if response.status_code == 200:
@@ -12,5 +12,6 @@ def set_value():
         print("POST request failed")
 
 if __name__ == '__main__':
-    set_value()
+    set_value("__T70BDAE66_REAL_.1f=20.0")
+    set_value("__T326B0D57_BOOL_i=1")
     
