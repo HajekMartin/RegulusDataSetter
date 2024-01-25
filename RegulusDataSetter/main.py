85f565d4fad3548a7a18e273da1a73d1485eecf9
Martin <martin.hajek1@tul.cz>
2024-01-25 18:42:56 +0100

    test 1

--- RegulusDataSetter/main.py
+++ RegulusDataSetter/main.py
@@ -3,15 +3,23 @@ import json
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import yaml
+import time
 
 def read_json(filepath):
-    with open(filepath, 'r') as file:
-        data = json.load(file)
-        return data
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            return data
+    except (FileNotFoundError, json.JSONDecodeError, Exception) as e:
+        print_log(f"Failed to read JSON file: {e}", "set_value")
+        return None
 
 def read_actual_time_actions():
+    print_log("Reading actual time actions", "set_value")
     current_date = datetime.date.today().strftime("%Y-%m-%d")
     schedule = read_json("./RegulusDataSetter/" + current_date + ".txt")
+    if schedule is None:
+        return
     # Key
     now = datetime.datetime.now()
     current_hour = now.hour
@@ -19,58 +27,68 @@ def read_actual_time_actions():
     # Get actions
     try:
         actions = schedule[f"{current_hour:02d}" + ":" + f"{current_minute:02d}" + ":00"]
-        #actions = schedule["12:00:00"]
     except KeyError:
+        print_log("No actions for this time " + f"{current_hour:02d}" + ":" + f"{current_minute:02d}" + ":00", "set_value")
         return
-    
     set_value(actions)
     
 def set_value(actions):
-    current_datetime = datetime.datetime.now()
-    print("[", current_datetime, "]", " Setting values")
-    
+    print_log("Setting value", "set_value")
     for action in actions:
         url = action["URL"]
         data = action["Body"]
         try:
+            print_log("--- Setted value " + url + " " + data, "set_value")
+            """
             response = requests.post(url, data=data)
             if response.status_code == 200:
-                print("POST request successful")
+                print_log("Setted value " + url + " " + data, "set_value")
             else:
-                print("POST request failed " + url + " " + data)
+                print_log("POST request failed " + url + " " + data, "set_value")
+            """
         except:
-            print("POST request failed " + url + " " + data)
+            print_log("POST request failed with exeption " + url + " " + data, "set_value")
             continue
 
 def download_schedule():
+    print_log("Download schedule starting", "download_schedule")
     guid = read_config('guid')
+    #guid = "8B12DFE1-6E3B-46E8-AF38-E1C0E73C2558"
     url = read_config('url')
+    #url = "https://dphajek-windows.azurewebsites.net/Api/ApiRegulus/GetTomorrowSchedulde"
     next_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    try:
-        params = {
-            'date': next_date,
-            'guid': guid
-        }
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        json_data = response.json()
-        file_name = next_date + ".txt"
-        with open(file_name, 'w') as file:
-            file.write(str(json_data))
+    next_date = (datetime.date.today()).strftime("%Y-%m-%d")
 
-        print(f"JSON data downloaded and saved to {file_name}")
-    except requests.RequestException as e:
-        print(f"Error occurred while downloading the JSON: {e}")
-    except IOError as e:
-        print(f"Error occurred while writing to the file: {e}")
+    while True:
+        print_log("Downloading schedule", "download_schedule")
+        try:
+            params = {
+                'date': next_date,
+                'guid': guid
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            json_data = response.json()
+            file_name = next_date + ".txt"
+            with open(file_name, 'w') as file:
+                file.write(str(json_data))
+            print_log(f"JSON data downloaded and saved to {file_name}" "download_schedule")
+            return
+        except (requests.RequestException, IOError) as e:
+            print_log(f"Failed to download schedule {e}" "download_schedule")
+            time.sleep(300)  # Wait for 5 minutes before next attempt
 
 def read_config(value_name):
+    print_log("Reding config " + value_name, "read_config")
     file_path = '/usr/src/app/config.yaml'
     with open(file_path, 'r') as config_file:
         config = yaml.safe_load(config_file)
 
     value = config['options'].get(value_name)
     return value
+
+def print_log(message, method):
+    print("[", datetime.datetime.now(), method, "]", message)
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
