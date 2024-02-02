01c3abf0ca630b50fd222887736731cd4541e52e
Martin <martin.hajek1@tul.cz>
2024-02-02 14:10:30 +0100

    Downloading schedule every 15 minutes

--- RegulusDataSetter/main.py
+++ RegulusDataSetter/main.py
@@ -17,29 +17,16 @@ def read_json(filepath):
         print_log(f"Failed to read JSON file: {e}", "set_value")
         return None
 
-def read_actual_time_actions():
-    print_log("Reading actual time actions", "set_value")
-    current_date = datetime.date.today().strftime("%Y-%m-%d")
-    schedule = read_json(current_date + ".txt")
-    if schedule is None:
-        return
-    # Key
-    now = datetime.datetime.now()
-    current_hour = now.hour
-    current_minute = now.minute
-    # Get actions
-    try:
-        actions = schedule[f"{current_hour:02d}" + ":" + f"{current_minute:02d}" + ":00"]
-    except KeyError:
-        print_log("No actions for this time " + f"{current_hour:02d}" + ":" + f"{current_minute:02d}" + ":00", "set_value")
-        return
+def set_actual_actions():
+    date = datetime.datetime.now()
+    actions = download_schedule(date)
     set_value(actions)
-    
+
 def set_value(actions):
     print_log("Setting value", "set_value")
     for action in actions:
         url = action["URL"]
-        data = action["Body"]
+        data = action["body"]
         try:
             print_log("--- Setted value " + url + " " + data, "set_value")
             """
@@ -53,51 +40,23 @@ def set_value(actions):
             print_log("POST request failed with exeption " + url + " " + data, "set_value")
             continue
 
-def download_schedule():
-    print_log("Download schedule starting", "download_schedule")
+def download_schedule(date):
     guid = read_config('guid')
-    guid = "8B12DFE1-6E3B-46E8-AF38-E1C0E73C2558"
+    #guid = '8b12dfe1-6e3b-46e8-af38-e1c0e73c2558'
     url = read_config('url')
-    url = "https://dphajek-windows.azurewebsites.net/Api/Regulus/GetSchedule"
-    next_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    actual_date = datetime.date.today().strftime("%Y-%m-%d")
-
-    dates = []
-
-    current_time = datetime.datetime.now().time()
-    if current_time > datetime.time(19, 00) and not schedule_file_exists(next_date + ".txt"):
-        dates.append(next_date)
-    
-    if not schedule_file_exists(actual_date + ".txt"):
-        dates.append(actual_date)
-
-    
-    for i in range (len(dates)):
-        cont = True
-        while cont:
-            date = dates[i]
-            print_log("Downloading schedule " + date, "download_schedule")
-            try:
-                params = {
-                    'date': date,
-                    'guid': guid
-                }
-                response = requests.get(url, params=params)
-                response.raise_for_status()
-                json_data = response.json()
-                file_name = date + ".txt"
-                with open(DEF_FODLER + file_name, 'w') as file:
-                    file.write(json.dumps(json_data))
-                print_log(f"JSON data downloaded and saved to {file_name}", "download_schedule")
-                cont = False
-            except (requests.RequestException, IOError) as e:
-                print_log(f"Failed to download schedule {e}", "download_schedule")
-                time.sleep(300)  # Wait for 5 minutes before next attempt
-
-def schedule_file_exists(file_name):
-    if os.path.exists(DEF_FODLER + file_name):
-        return True
-    return False
+    #url = 'https://dphajek-windows.azurewebsites.net/Api/Regulus/GetActualActions'
+    print_log("Downloading schedule ", "download_schedule")
+    try:
+        params = {
+            'guid': guid,
+            'time': time.strftime("%H:%M"),
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return json.loads(response.text)
+    except (requests.RequestException, IOError) as e:
+        print_log(f"Failed to download schedule {e}", "download_schedule")
+        return None
 
 def read_config(value_name):
     try:
@@ -138,13 +97,8 @@ if __name__ == '__main__':
     print_log("Starting RegulusDataSetter", "main")
 
     scheduler = BlockingScheduler()
-    scheduler.add_job(read_actual_time_actions, 'cron', minute='0,15,30,45')
-    scheduler.add_job(download_schedule, 'cron', hour=19, minute=0)
-    # Zapnu hned aby se kdyžtak stáhnul aktuiální rozvrh
-    current_time = datetime.datetime.now().time()
-    scheduler.add_job(download_schedule, 'date', run_date=datetime.datetime.now() + datetime.timedelta(seconds=5))
+    scheduler.add_job(set_actual_actions, 'cron', minute='0,15,30,45')
 
-    scheduler.start()
     scheduler.start()
 
     
